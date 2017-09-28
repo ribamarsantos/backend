25be5178d4d8c16a438ac9dae558e5c1f94b3e2a
Ribamar Santos <ribamaar@gmail.com>
2017-09-28 15:12:10 -0300

    criando rota count

--- my-money-app/src/config/database.js
+++ my-money-app/src/config/database.js
@@ -4,3 +4,6 @@ module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
 
 mongoose.Error.messages.general.required = "O atribuito '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = " O valor '{VALUE}' informado é menor que o limite mínimo de '{MIN}'. "
+mongoose.Error.messages.Number.max = " O valor '{VALUE}' informado é maior que o limite máximo de '{MAX}'. "
+mongoose.Error.messages.String.enum = " O valor '{VALUE}' não é válido para o atributo '{PATH}'. "
